2dc8ed0664409a50466718bf0417f8baf2eecdf4
ambarnett <andy.barnett09@gmail.com>
2021-07-26 12:04:22 -0500

    displaying login and register pages and very basic home page

--- src/components/CharliesChecklist.js
+++ src/components/CharliesChecklist.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { Register } from './auth/Register'
+import { Login } from './auth/Login'
 import "./CharliesChecklist.css"
 
 export const CharliesChecklist = () => (
     <>
-        <h2>Charlie's Checklist</h2>
+        <Route
+            render={() => {
+                if (sessionStorage.getItem("charlies_user")) {
+                    return (
+                        <>
+                            <h2>Charlie's Checklist</h2>
+                        </>
+                    )
+                } else {
+                    return <Redirect to="/login" />
+                }
+            }}
+        />
+        <Route path="/login">
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
     </>
 )
